f570cfd11166285ddf27028c0f2fa6f29f548149
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2024-09-10 18:53:32 +0300

    fixing navbar active

--- resources/views/public/includes/navbar_pages.blade.php
+++ resources/views/public/includes/navbar_pages.blade.php
@@ -16,19 +16,19 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-lg-5 me-lg-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('index')}}">Home</a>
+                                <a class="nav-link"  href="{{route('index')}}">Home</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('topics')}}">Topics Listing</a>
+                                <a class="nav-link {{ request()->routeIs('topics') ? 'active' : '' }}"  href="{{route('topics')}}">Topics Listing</a>
                             </li>
     
                             <li class="nav-item">
-                                <a class="nav-link active" href="{{route('contact')}}">Contact Us</a>
+                                <a class="nav-link {{ request()->routeIs('contact') ? 'active' : '' }}"  href="{{route('contact')}}">Contact Us</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('testimonials')}}">Our Client Says</a>
+                                <a class="nav-link {{ request()->routeIs('testimonials') ? 'active' : '' }}"  href="{{route('testimonials')}}">Our Client Says</a>
                             </li>
                         </ul>
 
