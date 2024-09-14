27e5c358085fdfd4ecf0cf0eac24cb57598c8baa
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2024-09-15 01:24:43 +0300

    Fix a spelling error in the btn in message details

--- resources/views/admin/message_details.blade.php
+++ resources/views/admin/message_details.blade.php
@@ -12,7 +12,7 @@
                           <div style="white-space: pre-line;">{{$message['content']}}</div>
                           <div class="text-md-end">
                               <a class="btn mt-4 btn-secondary text-white fs-5 fw-bold border-0 py-2 px-md-5" href="{{route('messages.index')}}">
-                                Back to All Messaages
+                                Back to All Messages
                               </a>
                             </div>
                         </article>
